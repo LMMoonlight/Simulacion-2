7c62abc7c2046de4738bc471387cee94621717d6
Laura Molina <lauramolinay@gmail.com>
2022-03-04 07:37:32 -0500

    Inicio cordenadas polares

--- Physics/Assets/Scripts/Polars/Polars.cs
+++ Physics/Assets/Scripts/Polars/Polars.cs
@@ -5,22 +5,32 @@ using UnityEngine;
 public class Polars : MonoBehaviour
 {
 
-    [SerializeField] float radious = 1, tetha = 0;
+    [SerializeField] float radious = 1, theta = 0;
+    [SerializeField] Vector2 polarCord; //X is radious, Y is theta.
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        polarCord.y += 0.01f;
+        DrawPolar(polarCord);
     }
 
-    private void DrawPolar(float radious, float tetha)
+    private void DrawPolar(Vector2 polarCord)
     {
-        Vector3 cartesian = new Vector3(Mathf.Cos(tetha), Mathf.Sign(tetha));
-        Debug.DrawLine(Vector3.zero, cartesian);
+        Vector3 cartesian = PolarToCartesian(polarCord);
+        Debug.DrawLine(Vector3.zero, cartesian, Color.yellow);
+    }
+
+    private Vector3 PolarToCartesian(Vector2 polar)
+    {
+
+        Vector3 cartesian = new Vector3(Mathf.Cos(polarCord.y), Mathf.Sin(polarCord.y));
+        cartesian *= polarCord.x;
+        return cartesian;       
     }
 }
