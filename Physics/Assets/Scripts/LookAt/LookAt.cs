e02aa5b93814f0ecaa98f07b6e937f4416dbcbf8
Laura Molina <lauramolinay@gmail.com>
2022-03-11 06:41:44 -0500

    Begin the LookAt metod using ArcTan

---
+++ Physics/Assets/Scripts/LookAt/LookAt.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = GetWorldMousePosition();
+    }
+
+    private Vector4 GetWorldMousePosition()
+    {
+        Camera camera = Camera.main;
+        Vector3 screenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
+        worldPos.z = 0;
+        return worldPos;
+    }
+}
--- Physics/Assets/Scripts/Polars/Polars.cs
+++ Physics/Assets/Scripts/Polars/Polars.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Polars : MonoBehaviour
 {
-
-    [SerializeField] float radious = 1, theta = 0;
     [SerializeField] Vector2 polarCord; //X is radious, Y is theta.
+
+    [Header("Angular")]
+    [SerializeField] float angularSpeed;
+    [SerializeField] float angularAceleration;
+
+    [Header("Radial speed")]
+    [SerializeField] float radialSpeed;
+    [SerializeField] float radialAceleration;
+
+    [Header("Borders")]
+    [SerializeField] float xBorder;
+    [SerializeField] float yBorder;
+
+    [SerializeField] GameObject sphere;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +29,35 @@ public class Polars : MonoBehaviour
     void Update()
     {
         polarCord.y += 0.01f;
+        polarCord.x += 0.001f;
+
+        RadialMovement();
+        sphere.transform.position = PolarToCartesian(polarCord);        
+        
         DrawPolar(polarCord);
+        CheckCollision();
     }
 
+    private void RadialMovement()
+    {
+        radialSpeed += radialAceleration * Time.deltaTime;
+        polarCord.x += radialSpeed * Time.deltaTime;
+
+        angularSpeed += angularAceleration * Time.deltaTime;
+        polarCord.y += angularSpeed * Time.deltaTime;
+    }
+
+
+    private void CheckCollision()
+    {
+        //cambiarlo mejor a que chequee si el radio es mayor o igual a los bordes.
+
+        if (sphere.transform.position.x >= xBorder || sphere.transform.position.x <= -xBorder ||
+            sphere.transform.position.y >= yBorder || sphere.transform.position.y <= -yBorder)
+        {
+            radialSpeed *= -1;
+        }
+    }
     private void DrawPolar(Vector2 polarCord)
     {
         Vector3 cartesian = PolarToCartesian(polarCord);
