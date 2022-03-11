36023beb5d262aa40aa8b8acc87e91b384a9e3c6
Laura Molina <lauramolinay@gmail.com>
2022-03-11 07:31:34 -0500

    Finished the LookAt

--- Physics/Assets/Scripts/LookAt/LookAt.cs
+++ Physics/Assets/Scripts/LookAt/LookAt.cs
@@ -13,7 +13,12 @@ public class LookAt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = GetWorldMousePosition();
+        Vector3 mousePos = GetWorldMousePosition();
+        //float slope = mousePos.y / mousePos.x;
+        //float radians = Mathf.Atan(slope);
+        Vector3 diference = mousePos - transform.localPosition;
+        float radians = Mathf.Atan2(diference.y, diference.x);
+        transform.localRotation = Quaternion.Euler(0f, 0f, radians * Mathf.Rad2Deg);
     }
 
     private Vector4 GetWorldMousePosition()
@@ -22,6 +27,7 @@ public class LookAt : MonoBehaviour
         Vector3 screenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane);
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
         worldPos.z = 0;
+        Debug.DrawLine(Vector3.zero, transform.position);
         return worldPos;
     }
 }
