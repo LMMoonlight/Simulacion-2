198edd450ace4a0314be53c5df460e011879868d
Laura Molina <lauramolinay@gmail.com>
2022-05-20 18:48:47 -0500

    Terminar arbol Procedural

--- Physics/Assets/Scripts/Procedural/Tree.cs
+++ Physics/Assets/Scripts/Procedural/Tree.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
+    [SerializeField] GameObject branchPrefab;
+
     [SerializeField]int totalLevels = 2;
     int currentLevel = 0;
-    [SerializeField]GameObject branchPrefab;
+    
     Queue<GameObject> frontier = new Queue<GameObject>();
+
+    [SerializeField] private float rootLenght = 4;
+    [SerializeField] private float currentLenght = -1;
+    [SerializeField] private float reductionLenghtPerLevel = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentLenght = rootLenght;
+
         GameObject root = Instantiate(branchPrefab, transform);
-        ++currentLevel;
+        SetBranchLenght(root, rootLenght);
+
         frontier.Enqueue(root);
-        Queue<string> queue = new Queue<string>();
-
-        CreateBranch(root, 45f);
-        CreateBranch(root, -45f);
-
         GenerateTree();
     }
 
@@ -27,6 +31,7 @@ public class Tree : MonoBehaviour
     {
         if (currentLevel >= totalLevels) return;
         ++currentLevel;
+        rootLenght = rootLenght - rootLenght * reductionLenghtPerLevel;
 
         List<GameObject> createdBranches = new List<GameObject>();
         while(frontier.Count > 0)
@@ -50,9 +55,29 @@ public class Tree : MonoBehaviour
     }
     private GameObject CreateBranch(GameObject prevBranch, float angle)
     {
-        GameObject branch = Instantiate(prevBranch);
-        branch.transform.position = prevBranch.transform.position + prevBranch.transform.up;
-        branch.transform.rotation *= Quaternion.Euler(0, 0, angle);
+        GameObject branch = Instantiate(branchPrefab, transform);
+
+        branch.transform.position = prevBranch.transform.position + prevBranch.transform.up * currentLenght;
+        Quaternion prevRotation = prevBranch.transform.rotation;
+
+        SetBranchLenght(branch, currentLenght);
+        prevRotation *= Quaternion.Euler(0, 0, angle);
+        branch.transform.rotation = prevRotation;
+
         return branch;
+    }
+
+    private void SetBranchLenght(GameObject branch, float lenght)
+    {
+        Transform line = branch.transform.GetChild(0);
+        Transform circle = branch.transform.GetChild(1);
+        line.localScale = new Vector3(line.localScale.x, lenght, line.localScale.z);
+        line.localPosition = new Vector3(0f, lenght * 0.5f, 0f);
+        circle.localPosition = new Vector3(0f, lenght, 0f);
+    }
+    private float GetBranchLength(GameObject branch)
+    {
+        Transform line = branch.transform.GetChild(0);
+        return line.localScale.y;
     }
 }
